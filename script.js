c800cab0d17b1ae8ee98847bfbf47b40d9d36646
kaderaid08 <karinar97@nycstudents.net>
2026-01-13 09:52:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,16 +2,38 @@ const inbox = document.querySelector("#heading");
 const subheading = document.querySelector("#subheading");
 const openButton = document.querySelector("#open-button");
 const unreadButton = document.querySelector("#mark-button");
+const markRead = document.querySelector("#mark-button");
+const entireInbox = document.querySelector("#inbox");
+const inboxMessage = document.querySelector("#inbox-message");
+const close = document.querySelector("#close");
 
-const markasRead = (e) =>{
-    e.target.classList.add("is-read");
-    inbox.classList.remove("hidden");
 
+// marks unread messages (Spicy Activity) - PART 1
+const markAsRead = (e) =>{
+    inboxMessage.classList.add("hidden");
+    entireInbox.classList.remove("is-read");
+    markRead.classList.add("hidden");
+}
+markRead.addEventListener("click", markAsRead);
+
+//open and closes message (Medium Activity) - PART 1
+
+const openMessage = (e) =>{
+    entireInbox.classList.add("is-read");
+    inboxMessage.classList.remove("hidden");
+    markRead.classList.remove("hidden");
 };
-openButton.addEventListener("click", markasRead);
+openButton.addEventListener("click", openMessage);
+
+const exit = (e) =>{
+    inboxMessage.classList.add("hidden");
+};
+close.addEventListener("click", exit);
+
+//change font color of heading when clicked
 
 const changeFontColor = (e) =>{
-    e.target.classList.add("blue-text");
+    e.target.classList.toggle("blue-text");
 };
 
 inbox.addEventListener("click", changeFontColor)
